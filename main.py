81856ad9186c8d40654ef6345aff8e990b6dd947
shotiko <lootb25@gmail.com>
2022-03-13 17:51:10 +0400

    upd

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ from pprint import pprint
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environe.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environe.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 db = SQLAlchemy(app)
 
 
